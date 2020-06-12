b661982cf7e7d3c69523e1baccbce1fbebde82b4
陈栋明 <chendm0602@163.com>
2020-06-12 11:45:47 +0800

    完成tab2页面

---
+++ h5/src/components/service/service.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <van-overlay :show="show"  class-name="ser">
+            <div @click.stop>
+                <div class="ser-content">
+                    <div class="ser-title">
+                        <div>微信扫码联系客服</div>
+                        <div @click="index===0?index=1:index=0">切换</div>
+                    </div>
+                    <img   v-for="(t,i) in imgs" :key="i" v-if="index===i" :src="t" alt="ser" @click="serEvent(i,t)" >
+                    <div class="ser-colse" @click="show = false"><van-icon name="cross" size="2rem" color="#bbb" /></div>
+                </div>
+            </div>
+        </van-overlay>
+    </div>
+</template>
+<script>
+import { ImagePreview } from 'vant';
+export default {
+    data(){
+        return{
+            show:false,
+            imgs:[
+                require('@/assets/imgs/ser/wx1.jpg'),
+                require('@/assets/imgs/ser/wx2.jpg')
+            ],
+            index:0
+        }
+    },
+    components: {
+        [ImagePreview.Component.name]: ImagePreview.Component,
+    },
+    methods:{
+        serEvent(i){
+            ImagePreview({
+                images:[
+                ...this.imgs
+                ],
+                startPosition:i,
+                closeable:true
+            });
+        },
+        showEvent(){
+            this.show=true;
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .ser{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 9999;
+    }
+    .ser-content{
+        width: 300px;
+        background: #fff;
+        padding: 15px 20px 10px;
+        border-radius: 16px;
+        img{
+            width: 200px;
+            height: 200px;
+            margin: 15px auto 0;
+            border-radius: 5px;
+            border: 1px solid $color;
+        }
+        .ser-colse{
+            text-align: center;
+            margin-top: 20px;
+        }
+    }
+    .ser-title{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        div:nth-child(1){
+            color: $color;
+        }
+        div:nth-child(2){
+            width: 45px;
+            text-align: center;
+            line-height: 24px;
+            font-size: .75rem;
+            background: $color;
+            color: $colorF;
+            border-radius: 12px;
+        }
+    }
+</style>
--- h5/src/pages/index/tab2.vue
+++ h5/src/pages/index/tab2.vue
@@ -1,15 +1,78 @@
 <template>
     <div>
-        我是Er
+        <div class="tab2-top">
+            <div>
+                <img  src="../../assets/imgs/all/logo.jpg" alt="logo" >
+                用户名
+            </div>
+        </div>
+        <div class="tab2-list">
+            <router-link to="">
+                <div class="tab2-item">
+                    <van-icon name="cart-circle" color="#62c8ca" size="1.8rem" />
+                    <div>我的订单</div>
+                </div>
+            </router-link>
+            <div class="tab2-item">
+                <van-icon name="manager" color="#62c8ca" size="1.8rem" />
+                <div @click="$refs.ser.showEvent()">我的客服</div>
+            </div>
+        </div>
+        <!-- 客服 -->
+        <Service ref="ser"></Service>
     </div>
 </template>
 <script>
+import Service from "@/components/service/service"
 export default {
     data(){
-        return{}
+        return{
+            show:true
+        }
+    },
+    components:{
+        Service
     }
 }
 </script>
 <style lang="scss" scoped>
-
+    .tab2-top{
+        width: 100%;
+        height: 230px;
+        background: linear-gradient(180deg,$color 0%,#fff 100%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding-bottom: 50px;
+        div{
+            // color: $colorF;
+            font-weight: bold;
+            letter-spacing: 5px;
+            font-size: 1rem;
+        }
+        img{
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+            margin-bottom: 15px;
+            border: 2px solid $colorF;
+        }
+    }
+    .tab2-list{
+        padding: 0 25px 0;
+        .tab2-item{
+            display: flex;
+            align-items: center;
+            border-top: 1px solid $border;
+            border-bottom: 1px solid $border;
+            padding: 5px;
+            margin-top: 20px;
+            >div{
+                font-weight: bold;
+                margin-left: 15px;
+                color: $color;
+            }
+        }
+    }
 </style>
